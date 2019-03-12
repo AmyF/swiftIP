400c404dc0678fc2aa9b33963c16142613d04054
unko <840382477@qq.com>
2019-03-13 00:45:17 +0800

    add ipv6, mask,update ip

--- Sources/swiftIP/swiftIP.swift
+++ Sources/swiftIP/swiftIP.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-private let v4InV6Prefix: [UInt8] = [0, 0, 0, 0 ,0, 0,0,0,0,0,0xff,0xff]
+private let _v4InV6Prefix: [UInt8] = [0, 0, 0, 0 ,0, 0,0,0,0,0,0xff,0xff]
+
+public protocol IPFormatter {
+    func string(from ip: IP) -> String
+}
 
 public protocol IP {
     var bytes: [UInt8] { get }
@@ -20,73 +24,91 @@ public protocol IP {
     var isInterfaceLocalMulticast: Bool { get }
     var isLinkLocalMulticast: Bool { get }
     var isLinkLocalUnicast: Bool { get }
-}
-
-public protocol IPFormatter {
-    func string(from ip: IP) -> String
-}
-
-public struct IPFormatStyle: IPFormatter, Equatable {
-    private enum Style {
-        case dotDecimal, dotHexadecimal, dotOctal, decimal, hexadecimal, octal
-    }
-    public static let dotDecimal = IPFormatStyle(style: .dotDecimal)
-    public static let dotHexadecimal = IPFormatStyle(style: .dotHexadecimal)
-    public static let dotOctal = IPFormatStyle(style: .dotOctal)
-    public static let decimal = IPFormatStyle(style: .decimal)
-    public static let hexadecimal = IPFormatStyle(style: .hexadecimal)
-    public static let octal = IPFormatStyle(style: .octal)
     
-    private let style: Style
-    
-    public func string(from ip: IP) -> String {
-        switch style {
-        case .dotDecimal: return ip.bytes.map{String($0)}.joined(separator: ".")
-        case .dotHexadecimal: return ip.bytes.map{String(format: "0x%02X", $0)}.joined(separator: ".")
-        case .dotOctal: return ip.bytes.map{String(format: "%04O", $0)}.joined(separator: ".")
-        case .decimal: return String(ip.bytes.reduce(0){($0 << 8) + Int($1)})
-        case .hexadecimal: return "0x" + String(ip.bytes.reduce(0){($0 << 8) + Int($1)}, radix: 16, uppercase: true)
-        case .octal: return "0" + String(ip.bytes.reduce(0){($0 << 8) + Int($1)}, radix: 8, uppercase: true)
-        }
-    }
+    func defaulMask() -> IPMask?
+    func masking(by mask: IPMask) -> IP?
 }
 
 public extension IP {
-    public static func ==(lhs: Self, rhs: Self) -> Bool {
-        switch (lhs.isIPv4, rhs.isIPv4) {
+    public func isEqual(to: IP) -> Bool {
+        switch (self.isIPv4, to.isIPv4) {
         case (true, true):
-            return lhs.bytes == rhs.bytes
+            return self.bytes == to.bytes
         case (true, false):
-            return [UInt8](rhs.bytes[...12]) == v4InV6Prefix && lhs.bytes == [UInt8](rhs.bytes[12...])
+            return [UInt8](to.bytes[...12]) == _v4InV6Prefix && self.bytes == [UInt8](to.bytes[12...])
         case (false, true):
-            return [UInt8](lhs.bytes[...12]) == v4InV6Prefix && rhs.bytes == [UInt8](lhs.bytes[12...])
+            return [UInt8](self.bytes[...12]) == _v4InV6Prefix && to.bytes == [UInt8](self.bytes[12...])
         case (false, false):
-            return lhs.bytes == rhs.bytes
+            return self.bytes == to.bytes
         }
     }
+    
+    public var isGlobalUnicast: Bool {
+        return (isIPv4 || isIPv6) &&
+            !isEqual(to: IPv4.boardcast) &&
+            !isUnspecified &&
+            !isLoopback &&
+            !isMulticast &&
+            !isLinkLocalUnicast
+    }
+    
+    public var isZero: Bool {
+        return bytes.contains(where: {$0 == 0})
+    }
+    
+    public var isUnspecified: Bool {
+        return (isIPv4 && isEqual(to: IPv4.zero)) || (isIPv6 && isEqual(to: IPv6.unspecified))
+    }
+    
+    public var isLoopback: Bool {
+        return (isIPv4 && bytes[0] == 127) || isEqual(to: IPv6.loopback)
+    }
+    
+    public var isMulticast: Bool {
+        return (isIPv4 && bytes[0] & 0xF0 == 0xE0) || (isIPv6 && bytes[0] == 0xFF)
+    }
+    
+    public var isInterfaceLocalMulticast: Bool {
+        return isIPv6 && (bytes[0] == 0xFF) && (bytes[1] & 0x0F == 0x01)
+    }
+    
+    public var isLinkLocalMulticast: Bool {
+        return (isIPv4 && bytes[0] == 224 && bytes[1] == 0 && bytes[2] == 0) ||
+            (isIPv6 && bytes[0] == 0xFF && bytes[1] & 0x0F == 0x02)
+    }
+    
+    public var isLinkLocalUnicast: Bool {
+        return (isIPv4 && bytes[0] == 169 && bytes[1] == 254) ||
+            (isIPv6 && bytes[0] == 0xFE && bytes[1] & 0xC0 == 0x80)
+    }
+    
+    public func defaulMask() -> IPMask? {
+        if let ip = toIPv4() {
+            return IPMask(ip: ip)
+        }
+        return nil
+    }
+    
+    public func masking(by mask: IPMask) -> IP? {
+        // TODO:
+        return nil
+    }
+    
 }
 
 public struct IPv4: IP {
-    static let zero = IPv4(from: [0,0,0,0])!
-    static let bcast = IPv4(from: [255,255,255,255])!
+    public static let zero = IPv4(from: [0,0,0,0])!
+    public static let boardcast = IPv4(from: [255,255,255,255])!
     
     public let lenght = 4
     public let bytes: [UInt8]
     
     init?(from: String) {
-        let segment = from.components(separatedBy: ".")
-        if (segment.contains{$0.count == 0}) || (segment.compactMap{Int($0)}.contains{$0 > 255}) {
-            return nil
-        }
-        
-        bytes = segment.compactMap { UInt8($0) }
-        if bytes.count != lenght {
-            return nil
-        }
+        self.init(from: from.components(separatedBy: ".").compactMap { UInt8($0) })
     }
     
     init?(from: [UInt8]) {
-        if from.count != 4 {
+        if from.count != lenght {
             return nil
         }
         bytes = from
@@ -105,54 +127,121 @@ public struct IPv4: IP {
     }
     
     public func toIPv6() -> IPv6? {
-        return IPv6()
+        return IPv6(from: _v4InV6Prefix + bytes)
     }
     
-    public func string(formatter: IPFormatter = IPFormatStyle.dotDecimal) -> String {
+    public func string(formatter: IPFormatter = Formatter.dotDecimal) -> String {
         return formatter.string(from: self)
     }
     
-    public func string(formatter: IPFormatStyle = .dotDecimal) -> String {
+    public func string(formatter: Formatter = .dotDecimal) -> String {
         return formatter.string(from: self)
-    }
-    
-    public var isGlobalUnicast: Bool {
-        return !(self == .bcast) &&
-            !isUnspecified &&
-            !isLoopback &&
-            !isMulticast &&
-            !isLinkLocalUnicast
-    }
-    
-    public var isZero: Bool {
-        return bytes.contains(where: {$0 == 0})
-    }
-    
-    public var isUnspecified: Bool {
-        return self == IPv4.zero
-    }
-    
-    public var isLoopback: Bool {
-        return bytes[0] == 127
-    }
-    
-    public var isMulticast: Bool {
-        return bytes[0] & 0xf0 == 0xe0
-    }
-    
-    public var isInterfaceLocalMulticast: Bool {
-        return false
-    }
-    
-    public var isLinkLocalMulticast: Bool {
-        return bytes[0] == 224 && bytes[1] == 0 && bytes[2] == 0
-    }
-    
-    public var isLinkLocalUnicast: Bool {
-        return bytes[0] == 169 && bytes[1] == 254
     }
 }
 
-public struct IPv6 {
+public extension IPv4 {
+    public struct Formatter: IPFormatter, Equatable {
+        private enum Style {
+            case dotDecimal, dotHexadecimal, dotOctal, decimal, hexadecimal, octal
+        }
+        public static let dotDecimal = Formatter(style: .dotDecimal)
+        public static let dotHexadecimal = Formatter(style: .dotHexadecimal)
+        public static let dotOctal = Formatter(style: .dotOctal)
+        public static let decimal = Formatter(style: .decimal)
+        public static let hexadecimal = Formatter(style: .hexadecimal)
+        public static let octal = Formatter(style: .octal)
+        
+        private let style: Style
+        
+        public func string(from ip: IP) -> String {
+            switch style {
+            case .dotDecimal: return ip.bytes.map{String($0)}.joined(separator: ".")
+            case .dotHexadecimal: return ip.bytes.map{String(format: "0x%02X", $0)}.joined(separator: ".")
+            case .dotOctal: return ip.bytes.map{String(format: "%04O", $0)}.joined(separator: ".")
+            case .decimal: return String(ip.bytes.reduce(0){($0 << 8) + Int($1)})
+            case .hexadecimal: return "0x" + String(ip.bytes.reduce(0){($0 << 8) + Int($1)}, radix: 16, uppercase: true)
+            case .octal: return "0" + String(ip.bytes.reduce(0){($0 << 8) + Int($1)}, radix: 8, uppercase: true)
+            }
+        }
+    }
+}
+
+public struct IPv6: IP {
+    public static let zero = IPv6(from: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])!
     
+    public static let unspecified = IPv6(from: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])!
+    
+    public static let loopback = IPv6(from: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])!
+    
+    public static let interfacelocalallnodes = IPv6(from: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])!
+    
+    public static let linklocalallnodes = IPv6(from: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])!
+    
+    public static let linklocalallrouters = IPv6(from: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])!
+    
+    public let lenght = 16
+    public let bytes: [UInt8]
+    
+    init?(from: String) {
+        // TODO:
+        return nil
+    }
+    
+    init?(from: [UInt8]) {
+        // TODO:
+        return nil
+    }
+    
+    public var isIPv4: Bool {
+        return false
+    }
+    
+    public var isIPv6: Bool {
+        return true
+    }
+    
+    public func toIPv4() -> IPv4? {
+        // TODO:
+        return nil
+    }
+    
+    public func toIPv6() -> IPv6? {
+        return self
+    }
+    
+    public func string(formatter: IPFormatter) -> String {
+        return formatter.string(from: self)
+    }
+}
+
+public extension IPv6 {
+    public struct Formatter: IPFormatter, Equatable {
+        public func string(from ip: IP) -> String {
+            // TODO:
+            return ""
+        }
+    }
+}
+
+public struct IPMask {
+    public static let aMask = IPMask(bytes: [0xff, 0, 0, 0])
+    public static let bMask = IPMask(bytes: [0xff, 0xff, 0, 0])
+    public static let cMask = IPMask(bytes: [0xff, 0xff, 0xff, 0])
+    
+    public let bytes: [UInt8]
+    
+    private init(bytes: [UInt8]) {
+        self.bytes = bytes
+    }
+    
+    init(ip: IPv4) {
+        switch ip.bytes[0] {
+        case ...0x80:
+            self = .aMask
+        case ...0xC0:
+            self = .bMask
+        default:
+            self = .cMask
+        }
+    }
 }
